062b0d69bb27de01f98c9a6f983a211e38bc7b5c
haoranw <imhaoran.wang@foxmail.com>
2016-09-11 22:01:36 +0800

    add action

--- js/common-compiled.js
+++ js/common-compiled.js
@@ -4,19 +4,29 @@
  * Created by wang on 16/9/7.
  */
 $(function () {
+
+    /*----搜索----*/
     var input = $(".search input");
     input.on({
         focus: function focus() {
-            if (this.value == "挑选您心仪的装修案例") {
+            if (this.value == "为您精心挑选") {
                 this.value = "";
             }
         },
         blur: function blur() {
             if (this.value == "") {
-                this.value = "挑选您心仪的装修案例";
+                this.value = "为您精心挑选";
             }
         }
     });
+    $(".search-list").hover(function () {
+        $(".search-ul").css("display", "block").click(function (event) {
+            $(".search-list").find("span").text($(event.target).text());
+        });
+    }, function () {
+        $(".search-ul").css("display", "none");
+    });
+
     $(".free-head-r").click(function () {
         $(this).addClass("active");
         $(".free-head-l").removeClass("active");
@@ -39,82 +49,164 @@ $(function () {
             }
         }
     });
-    /*----轮播------*/
-    //banner config
 
-    var banner_config = {
-        num: 3,
-        time: 3000
-    };
+    /*---------导航-------------*/
+    $(".outer-li").hover(function () {
+        $(this).find(".nav-slide").css("display", "block");
+    }, function () {
+        $(this).find(".nav-slide").css("display", "none");
+    });
+
+    $(".app").hover(function () {
+        $(".appqr").css("display", "block");
+    }, function () {
+        $(".appqr").css("display", "none");
+    });
+
+    /*----------轮播----------*/
+
     {
         (function () {
-            var i = 0;
-            setInterval(function () {
-                var li = $(".banner-img li");
-                if (i < banner_config.num - 1) {
-                    li.eq(i).animate({
-                        opacity: 0
-                    }, 400, function () {
-                        li.eq(i).index = 0;
+            var banner = $(".banner-img");
+            var li = banner.find("li");
+            var dot = $(".dot");
+            //轮播插件 by 王浩然
+            {
+                (function () {
+
+                    //计时器
+                    var start = function start() {
+                        timer = setInterval(function () {
+                            move();
+                        }, banner_config.time);
+                    };
+
+                    //切换动画
+
+
+                    var animate = function animate(from, to) {
+                        banner_config.banner.eq(from).animate({
+                            opacity: 0
+                        }, 400, function () {
+                            banner_config.banner.eq(from).css("z-index", 0);
+                        });
+                        banner_config.index.eq(from).removeClass("on");
+                        banner_config.index.eq(to).addClass("on");
+                        banner_config.banner.eq(to).animate({
+                            opacity: 1
+                        }, 400, function () {
+                            banner_config.banner.eq(to).css("z-index", 1);
+                        });
+                    };
+
+                    var move = function move() {
+                        if (i < banner_config.num - 1) {
+                            animate(i, i + 1);
+                            i++;
+                        } else {
+                            animate(i, 0);
+                            i = 0;
+                        }
+                    };
+
+                    var banner_config = {
+                        num: 3, //轮播图数量
+                        time: 3000, //动画时间
+                        banner: li, //对应的各个图片外层节点
+                        index: dot };
+                    var i = 0;
+                    var timer = void 0;
+
+                    //鼠标进出事件
+                    banner.on({
+                        mouseenter: function mouseenter() {
+                            $(".prev,.next").fadeTo("show", 0.5);
+                            clearInterval(timer);
+                        },
+                        mouseleave: function mouseleave() {
+                            $(".prev,.next").fadeTo("show", 0);
+                            start();
+                        }
                     });
-                    li.eq(i + 1).animate({
-                        opacity: 1
-                    }, 400, function () {
-                        li.eq(i).index = 1;
+                    //前后箭头点击事件
+                    $(".prev").click(function () {
+                        if (i > 0) {
+                            banner_config.banner.eq(i).animate({
+                                opacity: 0
+                            }, 400, function () {
+                                li.eq(i).index = 0;
+                            });
+                            banner_config.index.eq(i).removeClass("on");
+                            banner_config.index.eq(i - 1).addClass("on");
+                            banner_config.banner.eq(i - 1).animate({
+                                opacity: 1
+                            }, 400, function () {
+                                banner_config.banner.eq(i).index = 1;
+                            });
+                            i--;
+                        } else {
+                            banner_config.banner.eq(i).animate({
+                                opacity: 0
+                            }, 400, function () {
+                                banner_config.banner.eq(i).index = 0;
+                            });
+                            banner_config.index.eq(i).removeClass("on");
+                            banner_config.index.eq(banner_config.num - 1).addClass("on");
+                            banner_config.banner.eq(banner_config.num - 1).animate({
+                                opacity: 1
+                            }, 400, function () {
+                                banner_config.banner.eq(i).index = 1;
+                            });
+                            i = banner_config.num - 1;
+                        }
                     });
-                    i++;
-                } else {
-                    li.eq(i).animate({
-                        opacity: 0
-                    }, 400, function () {
-                        li.eq(i).index = 0;
+                    $(".next").click(function () {
+                        move();
                     });
-                    li.eq(0).animate({
-                        opacity: 1
-                    }, 400, function () {
-                        li.eq(i).index = 1;
-                    });
-                    i = 0;
-                }
-            }, banner_config.time);
+                    //index的点击事件
+
+                    $(".banner-dot li").hover(function (event) {
+                        var number = $(".banner-dot li").index(event.target);
+                        if (i != number) {
+                            //目标是当前不进行切换
+                            animate(i, number);
+                            i = number;
+                        }
+                    }, function () {});
+
+                    start();
+                })();
+            }
         })();
     }
+
     /*---step广告hover---*/
 
-    var _loop = function _loop(_i) {
-        $(".step" + _i).hover(function () {
-            $(".step" + _i + " img").eq(0).addClass("hid");
-            $(".step" + _i + " img").eq(1).removeClass("hid");
+    for (var i = 1; i < 11; i++) {
+        $(".step" + i).hover(function () {
+            $(this).find("img").eq(0).addClass("hid");
+            $(this).find("img").eq(1).removeClass("hid");
         }, function () {
-            $(".step" + _i + " img").eq(1).addClass("hid");
-            $(".step" + _i + " img").eq(0).removeClass("hid");
+            $(this).find("img").eq(1).addClass("hid");
+            $(this).find("img").eq(0).removeClass("hid");
         });
-    };
-
-    for (var _i = 1; _i < 10; _i++) {
-        _loop(_i);
     }
-
     /*---foot-nav---*/
-    {
-        (function () {
-            var init = function init() {
-                for (var _i3 = 0; _i3 < foot_nav.length; _i3++) {
-                    $(".friend-list").eq(_i3).removeClass("friend-show");
-                    foot_nav.eq(_i3).removeClass("hov");
-                }
-            };
+    var foot_nav = $(".friend-nav a");
+    for (var _i = 0; _i < foot_nav.length; _i++) {
+        foot_nav.eq(_i).data("num", _i);
+    }
+    foot_nav.hover(function () {
+        init();
+        $(this).addClass("hov");
+        $(".friend-list").eq($(this).data("num")).addClass("friend-show");
+    }, function () {});
 
-            var foot_nav = $(".friend-nav a");
-            for (var _i2 = 0; _i2 < foot_nav.length; _i2++) {
-                foot_nav.eq(_i2).data("num", _i2);
-            }
-            foot_nav.hover(function () {
-                init();
-                $(this).addClass("hov");
-                $(".friend-list").eq($(this).data("num")).addClass("friend-show");
-            }, function () {});
-        })();
+    function init() {
+        for (var _i2 = 0; _i2 < foot_nav.length; _i2++) {
+            $(".friend-list").eq(_i2).removeClass("friend-show");
+            foot_nav.eq(_i2).removeClass("hov");
+        }
     }
 }); //end
 
--- js/common.js
+++ js/common.js
@@ -2,19 +2,33 @@
  * Created by wang on 16/9/7.
  */
 $(function () {
+
+    /*----搜索----*/
     let input = $(".search input");
     input.on({
         focus: function () {
-            if (this.value == "挑选您心仪的装修案例") {
+            if (this.value == "为您精心挑选") {
                 this.value = "";
             }
         },
         blur: function () {
             if (this.value == "") {
-                this.value = "挑选您心仪的装修案例";
+                this.value = "为您精心挑选";
             }
         }
     });
+    $(".search-list").hover(function () {
+        $(".search-ul").css("display", "block").click(function(event){
+            $(".search-list").find("span").text(
+                $(event.target).text());
+            }
+        );
+    }, function () {
+        $(".search-ul").css("display", "none");
+    });
+
+
+
     $(".free-head-r").click(function () {
         $(this).addClass("active");
         $(".free-head-l").removeClass("active");
@@ -37,74 +51,161 @@ $(function () {
             }
         }
     });
-    /*----轮播------*/
-    //banner config
 
-    let banner_config = {
-        num: 3,
-        time: 3000,
-    };
+
+    /*---------导航-------------*/
+    $(".outer-li").hover(function () {
+        $(this).find(".nav-slide").css("display", "block");
+    }, function () {
+        $(this).find(".nav-slide").css("display", "none");
+    });
+
+    $(".app").hover(function () {
+        $(".appqr").css("display", "block");
+    },function () {
+        $(".appqr").css("display", "none");
+    })
+
+
+    /*----------轮播----------*/
+
     {
-        let i = 0;
-        setInterval(function () {
-            let li = $(".banner-img li");
-            if (i < banner_config.num - 1) {
-                li.eq(i).animate({
-                    opacity: 0,
-                }, 400, function () {
-                    li.eq(i).index = 0
-                });
-                li.eq(i + 1).animate({
-                    opacity: 1,
-                }, 400, function () {
-                    li.eq(i).index = 1
-                });
-                i++;
-            } else {
-                li.eq(i).animate({
-                    opacity: 0,
-                }, 400, function () {
-                    li.eq(i).index = 0
-                });
-                li.eq(0).animate({
-                    opacity: 1,
-                }, 400, function () {
-                    li.eq(i).index = 1
-                });
-                i = 0;
+        let banner = $(".banner-img");
+        let li = banner.find("li");
+        let dot = $(".dot");
+        //轮播插件 by 王浩然
+        {
+            let banner_config = {
+                num: 3,      //轮播图数量
+                time: 3000,  //动画时间
+                banner: li,   //对应的各个图片外层节点
+                index: dot,    //下方的索引点
+            };
+            let i = 0;
+            let timer;
+
+            //鼠标进出事件
+            banner.on({
+                mouseenter: function () {
+                    $(".prev,.next").fadeTo("show", 0.5);
+                    clearInterval(timer);
+                },
+                mouseleave: function () {
+                    $(".prev,.next").fadeTo("show", 0);
+                    start();
+                }
+            });
+            //前后箭头点击事件
+            $(".prev").click(function () {
+                if (i > 0) {
+                    banner_config.banner.eq(i).animate({
+                        opacity: 0,
+                    }, 400, function () {
+                        li.eq(i).index = 0
+                    });
+                    banner_config.index.eq(i).removeClass("on");
+                    banner_config.index.eq(i - 1).addClass("on");
+                    banner_config.banner.eq(i - 1).animate({
+                        opacity: 1,
+                    }, 400, function () {
+                        banner_config.banner.eq(i).index = 1
+                    });
+                    i--;
+                } else {
+                    banner_config.banner.eq(i).animate({
+                        opacity: 0,
+                    }, 400, function () {
+                        banner_config.banner.eq(i).index = 0
+                    });
+                    banner_config.index.eq(i).removeClass("on");
+                    banner_config.index.eq(banner_config.num - 1).addClass("on");
+                    banner_config.banner.eq(banner_config.num - 1).animate({
+                        opacity: 1,
+                    }, 400, function () {
+                        banner_config.banner.eq(i).index = 1
+                    });
+                    i = banner_config.num - 1;
+                }
+            });
+            $(".next").click(function () {
+                move();
+            });
+            //index的点击事件
+
+            $(".banner-dot li").hover(function (event) {
+                let number = $(".banner-dot li").index(event.target);
+                if (i != number) {            //目标是当前不进行切换
+                    animate(i, number);
+                    i = number;
+                }
+            }, function () {
+            });
+
+            //计时器
+            function start() {
+                timer = setInterval(function () {
+                    move();
+                }, banner_config.time)
             }
-        }, banner_config.time)
+
+            //切换动画
+            function animate(from, to) {
+                banner_config.banner.eq(from).animate({
+                    opacity: 0,
+                }, 400, function () {
+                    banner_config.banner.eq(from).css("z-index", 0)
+                });
+                banner_config.index.eq(from).removeClass("on");
+                banner_config.index.eq(to).addClass("on");
+                banner_config.banner.eq(to).animate({
+                    opacity: 1,
+                }, 400, function () {
+                    banner_config.banner.eq(to).css("z-index", 1)
+                });
+            }
+
+            function move() {
+                if (i < banner_config.num - 1) {
+                    animate(i, i + 1);
+                    i++;
+                } else {
+                    animate(i, 0);
+                    i = 0;
+                }
+            }
+
+            start();
+        }
     }
+
     /*---step广告hover---*/
-    for (let i = 1; i < 10; i++) {
+
+    for (let i = 1; i < 11; i++) {
         $(".step" + i).hover(function () {
-            $(".step" + i + " img").eq(0).addClass("hid");
-            $(".step" + i + " img").eq(1).removeClass("hid");
+            $(this).find("img").eq(0).addClass("hid");
+            $(this).find("img").eq(1).removeClass("hid");
         }, function () {
-            $(".step" + i + " img").eq(1).addClass("hid");
-            $(".step" + i + " img").eq(0).removeClass("hid");
+            $(this).find("img").eq(1).addClass("hid");
+            $(this).find("img").eq(0).removeClass("hid");
         });
     }
-
-
     /*---foot-nav---*/
-    {
-        let foot_nav = $(".friend-nav a");
-        for (let i = 0; i < foot_nav.length; i++) {
-            foot_nav.eq(i).data("num", i);
-        }
-        foot_nav.hover(function () {
-            init();
-            $(this).addClass("hov");
-            $(".friend-list").eq($(this).data("num")).addClass("friend-show");
-        }, function () {
-        });
+    let foot_nav = $(".friend-nav a");
+    for (let i = 0; i < foot_nav.length; i++) {
+        foot_nav.eq(i).data("num", i);
+    }
+    foot_nav.hover(function () {
+        init();
+        $(this).addClass("hov");
+        $(".friend-list").eq($(this).data("num")).addClass("friend-show");
+    }, function () {
+    });
 
-        function init(){
-            for (let i=0;i<foot_nav.length;i++){
-                $(".friend-list").eq(i).removeClass("friend-show");
-                foot_nav.eq(i).removeClass("hov");
-            }
+    function init() {
+        for (let i = 0; i < foot_nav.length; i++) {
+            $(".friend-list").eq(i).removeClass("friend-show");
+            foot_nav.eq(i).removeClass("hov");
         }
     }
-});//end
+})
+;//end
